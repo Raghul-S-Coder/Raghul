9206c742f750fd3072763442e2e36f27b5e0abb9
Raghul S <48400310+Raghul-S-Coder@users.noreply.github.com>
2019-07-11 20:57:28 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,20 @@
+
+var app = angular.module('Ap', []);
+
+app.controller('myAp', function($scope) {
+  $scope.page = {
+     heading1:"Welcome To My Site",
+     name:"Raghul S",
+     phone:"8012364767",
+     email:"eceraghulksrct@gmail.com",
+     aboutP:"Click to see the resume online",
+     ProTitle:"Project Details",
+     down:"Download APK",
+     jq:" ( jQuery )",
+     andro:" Android Studio (java) ",
+     pro1:"Calculator",
+     pro2:"Shifting number",
+     pro3:"Moving Game",
+  };
+  
+});
